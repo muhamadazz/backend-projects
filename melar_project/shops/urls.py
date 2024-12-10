1c10e64e7326bded73ab89d0250d5328bd5f5f5c
fahraz <fahrazfirdaus@gmail.com>
2024-12-10 21:36:12 +0700

    penyesuasian shops

--- melar_project/shops/urls.py
+++ melar_project/shops/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter # type: ignore
 from . import views
 
 router = DefaultRouter()
 router.register(r'shops', views.ShopViewSet)  # 'shops' adalah bagian dari URL
+router.register(r'products', views.ProductViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'discounts', views.DiscountViewSet)
+router.register(r'inventory', views.InventoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),  # URL dasar untuk mengakses router
